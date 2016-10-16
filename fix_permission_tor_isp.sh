4f65f0ebcbaf69e3f3ad45552d6ae21e2ca7c054
ReaGed <admin@freehacks.ru>
2016-10-16 06:32:47 +0300

    v.2.0.0
    Изменен алгоритм, теперь меняется не маска доступа к директориям, а пользователь и группа.

--- fix_permission_tor_isp.sh
+++ fix_permission_tor_isp.sh
@@ -14,13 +14,11 @@ Running(){
 		echo `date +"%h %d %T.000"` "[notice] ${GR}# Tor: running #${RS}"
 		echo `date +"%h %d %T.000"` "[notice] ${GR}# Tor: running #${RS} @ Quareal/fix_permission_tor_isp" >> /var/log/tor/log
 		chmod 0700 /var/www/{ISP_USER}/data/www/*
-		chown debian-tor:{ISP_USER} /var/www/{ISP_USER}/data/www/
-			sleep 15
+		chown debian-tor:{ISP_USER} /var/www/{ISP_USER}/data/www/*
 		/etc/init.d/tor force-reload &
-			sleep 5
+			sleep 1
 		if [ $( IfRun ) == "1" ]; then
-			chmod 0755 /var/www/{ISP_USER}/data/www/*
-				sleep 15
+			chown {ISP_USER}:debian-tor /var/www/{ISP_USER}/data/www/*
 			echo `date +"%h %d %T.000"` "[notice] ${GR}force-reload completed${RS}"
 			echo `date +"%h %d %T.000"` "[notice] ${GR}force-reload completed${RS} @ Quareal/fix_permission_tor_isp" >> /var/log/tor/log
 		else
@@ -33,13 +31,12 @@ NotRunning(){
 		echo `date +"%h %d %T.000"` "[warn] ${RD}# Tor: not running #${RS}"
 		echo `date +"%h %d %T.000"` "[warn] ${RD}# Tor: not running #${RS} @ Quareal/fix_permission_tor_isp" >> /var/log/tor/log
 		chmod 0700 /var/www/{ISP_USER}/data/www/*
-		chown debian-tor:{ISP_USER} /var/www/{ISP_USER}/data/www/
+		chown debian-tor:{ISP_USER} /var/www/{ISP_USER}/data/www/*
 		    sleep 15
 		/etc/init.d/tor start &
-			sleep 5
+			sleep 1
 		if [ $( IfRun ) == "1" ]; then
-			chmod 0755 /var/www/{ISP_USER}/data/www/*
-				sleep 15
+			chown {ISP_USER}:debian-tor /var/www/{ISP_USER}/data/www/*
 			echo `date +"%h %d %T.000"` "[notice] ${GR}start completed${RS}"
 			echo `date +"%h %d %T.000"` "[notice] ${GR}start completed${RS} @ Quareal/fix_permission_tor_isp" >> /var/log/tor/log
 		else
